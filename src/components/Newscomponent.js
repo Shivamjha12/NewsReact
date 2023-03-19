1926328cb880a85250a3b1a3677599a39247a3e8
Shivamjha12 <Jhasapna544@gmail.com>
2023-03-20 00:39:34 +0530

    20/03/2024 12:36am

--- src/components/Newscomponent.js
+++ src/components/Newscomponent.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 export default class Newscomponent extends Component {
+
+  constructor(){
+    super();
+    console.log("Shivam Jha");
+    this.state = {
+      articles: [],
+      loaded:false,
+      page:1
+    }
+   }
+    async componentDidMount(){
+      let url = "https://newsapi.org/v2/everything?q=travel&from=2023-03-15&to=2023-03-15&sortBy=popularity&apiKey=44cbf34cc91942cb9ddd9c45ff723a75&page=1&pagesize=12";
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+
+   }
+
+   handlePreviousClick = async ()=>{
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/everything?q=travel&from=2023-03-15&to=2023-03-15&sortBy=popularity&apiKey=44cbf34cc91942cb9ddd9c45ff723a75&pagesize=12&page=${this.state.page -1}`;
+    console.log(url);
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+        page:this.state.page-1,
+        articles: parsedData.articles
+        
+      })
+   }
+
+   handleNextClick = async ()=>{
+    if(this.state.page>Math.ceil(this.state.totalResults/20)){
+      console.log("There is no more data to see");
+    }
+    else{
+      let url = `https://newsapi.org/v2/everything?q=travel&from=2023-03-15&to=2023-03-15&sortBy=popularity&apiKey=44cbf34cc91942cb9ddd9c45ff723a75&pagesize=12&page=${this.state.page+1}`;
+      console.log(url);
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+          page:this.state.page+1,
+          articles: parsedData.articles
+          
+        })
+     }
+   }
+
   render() {
     return (
       <div className="container my-3">
-        This is Newscomponent
+        <h1>Lates News</h1>
         <div className="row">
-        <div className="col-md-4">
-        <Newsitem title="Hello hii i am shivam" description="Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha" imagelink="https://miro.medium.com/v2/resize:fit:490/1*33PkguILLO7GBsdbGlSx3w.gif"/>
-        </div>
-        <div className="col-md-4">
-        <Newsitem title="Hello hii i am shivam" description="Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha" imagelink="https://miro.medium.com/v2/resize:fit:490/1*33PkguILLO7GBsdbGlSx3w.gif"/>
-        </div>
-        <div className="col-md-4">
-        <Newsitem title="Hello hii i am shivam" description="Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha" imagelink="https://miro.medium.com/v2/resize:fit:490/1*33PkguILLO7GBsdbGlSx3w.gif"/>
-        </div>
-        <div className="col-md-4">
-        <Newsitem title="Hello hii i am shivam" description="Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha Blaha blaha blaha" imagelink="https://miro.medium.com/v2/resize:fit:490/1*33PkguILLO7GBsdbGlSx3w.gif"/>
-        </div>
-        </div>
-
+        {this.state.articles.map((newsElements)=>{
+          return <div className="col-md-4" key={newsElements.url}>
+          <Newsitem title={newsElements.title?newsElements.title.slice(0,35):""} description={newsElements.description.slice(0,80)} imagelink={newsElements.urlToImage} newsurl={newsElements.url}/>
+          </div>
+          
+        })}
+      </div>
+      <div className="d-flex justify-content-between">
+        <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlePreviousClick} > &#8592; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &#8594; </button>
+      </div>
+      
       </div>
       
     )
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export default class Newsitem extends Component {
   render() {
-    let {title,description,imagelink} = this.props
+    let {title,description,imagelink,newsurl} = this.props
     return (
       <div>
-            <div className="card" style={{width: "18rem"}}>
-                <img className="card-img-top" src={imagelink} alt="Card image cap"/>
+            <div className="card my-2" style={{height:"25rem", width: "15rem"}}>
+                <img className="card-img-top" style={{height:"10rem", width: "15rem"}} src={!imagelink?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlS2MyP82wFMKpr7e1CxfyAoqgDRx0Bg0seg&usqp=CAU":imagelink} alt={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlS2MyP82wFMKpr7e1CxfyAoqgDRx0Bg0seg&usqp=CAU"}/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="/something" className="btn btn-primary">Go somewhere</a>
+                    <a href={newsurl} className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
       </div>
